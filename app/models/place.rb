a1e264df6eeec4ec87d8495c038ad012b7291e3f
Andre Yeargins <lonier4@gmail.com>
2019-10-04 19:16:18 +0000

    Added geocoder gem for maps capability converting address to lat and lng, installed figaro gem for security enabling ENV variable to ignore API key in git hub

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,4 +1,10 @@
 class Place < ApplicationRecord
   belongs_to :user
-  validates :name, presence: true
+  geocoded_by :address
+  after_validation :geocode
+  
+  validates :name, presence: true, length: {in: 3..100}
+  validates :address, presence: true, length: {in: 6..100}
+  validates :description, presence: true
+
 end
