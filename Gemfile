8fedaaa74257c6578a43048b6cf56f4175f0bd17
Andre Yeargins <lonier4@gmail.com>
2019-10-11 19:53:56 +0000

    Added photo modal but need to fix, added mailer to send emails when a comment is added

--- Gemfile
+++ Gemfile
@@ -69,6 +69,8 @@ gem "font-awesome-rails"
 gem 'devise'
 gem "geocoder"
 gem "figaro"
+gem 'carrierwave'
+gem 'fog-aws'
 
 source 'https://rails-assets.org' do
   gem 'rails-assets-tether', '>= 1.3.3'
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :places do
     resources :comments, only: :create
+    resources :photos, only: :create
   end
 end
